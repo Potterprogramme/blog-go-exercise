3e69ff3540bc31953b3f1e663bde9b7e27f8bedc
sorcerer <potterprograme@gmail.com>
2025-08-22 13:26:36 +0800

    start server

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	server := http.Server {
+		Addr : "127.0.0.1:8080",
+	}	
+	if err := server.ListenAndServe(); err != nil {
+		log.Println("Error starting server:", err)
+	}
+}
